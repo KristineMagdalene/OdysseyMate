622b2c732a735a51e55a8e023756387e205086e6
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2023-09-26 11:05:26 +0800

    Initial Commit

--- app/src/main/java/com/example/planner/MainActivity.java
+++ app/src/main/java/com/example/planner/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.planner;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +7,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toolbar;
-
+import com.example.planner.R;
 import com.example.planner.Fragment.AboutAppFragment;
 import com.example.planner.Fragment.HistoryFragment;
 import com.example.planner.Fragment.HomeFragment;
 import com.example.planner.databinding.ActivityMainBinding;
-import com.example.planner.databinding.FragmentHistoryBinding;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -38,28 +36,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         if (savedInstanceState == null ){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
-            navigationView.setCheckedItem(R.id.navHome);
+            navigationView.setCheckedItem(R.id.nav_Home);
 
 
         }
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.navHome:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
-                break;
-            case R.id.navAbout:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new AboutAppFragment()).commit();
-                break;
-            case R.id.navHistory:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HistoryFragment()).commit();
-                break;
-        }
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
+
 
     @Override
     public void onBackPressed() {
@@ -70,5 +53,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         else{
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.nav_Home) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        } else if (itemId == R.id.nav_History) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HistoryFragment()).commit();
+        } else if (itemId == R.id.nav_About) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutAppFragment()).commit();
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
